1550bba1bdd95184e5d43ed02e4a99f7a193dd01
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-04-29 15:36:59 -0700

    add videos dummy data

--- client/components/Videos.js
+++ client/components/Videos.js
@@ -1,4 +1,22 @@
 const React = require('react');
+const Sidebar = require('./Sidebar');
+const VideoLabeler = require('./VideoLabeler');
+
+const videos = [
+  { source: 'https://www.youtube.com/embed/gFKl6GvM27U'
+  },
+  { source: 'https://www.youtube.com/embed/gFKl6GvM27U'
+  },
+  { source: 'https://www.youtube.com/embed/gFKl6GvM27U'
+  },
+  { source: 'https://www.youtube.com/embed/gFKl6GvM27U'
+  },
+  { source: 'https://www.youtube.com/embed/gFKl6GvM27U'
+  },
+  { source: 'https://www.youtube.com/embed/gFKl6GvM27U'
+  }
+];
+
 
 class Videos extends React.Component {
 
@@ -10,9 +28,19 @@ class Videos extends React.Component {
     };
   }
 
+  handleClick (video) {
+    this.setState({
+      selected: video
+    });
+  }
+
+
   render () {
     return (
-      <div>Hello World</div>
+    <div>
+      <Sidebar handleClick={this.handleClick} media={videos} mediaType={'video'}/>
+      <VideoLabeler video={this.state.selected} />
+    </div>
     );
   }
 }
