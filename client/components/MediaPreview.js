58274de9333966b9bc7b07fcde4c81208d048689
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-04-28 22:38:57 -0700

    make images selectable from sidebar

--- client/components/MediaPreview.js
+++ client/components/MediaPreview.js
@@ -1,7 +1,11 @@
 const React = require('react');
 
-const NavEntry = (props) => (
-  <img className="responsive-img z-depth-1" src={props.media.source}/>
+const MediaPreview = (props) => (
+  <div className="card media" onClick={() => props.handleClick(props.media.source)}>
+    <div className="card-image">
+      <img className="z-depth-1" src={props.media.source}/>
+    </div>
+  </div>
 );
 
-module.exports = NavEntry;
+module.exports = MediaPreview;
--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,26 +1,18 @@
 const React = require('react');
 const MediaPreview = require('./MediaPreview');
-const media = [
-  { source: './../assets/socialmedia1.jpg'
-  },
-  { source: './../assets/socialmedia2.jpg'
-  },
-  { source: './../assets/socialmedia3.jpg'
-  },
-  { source: './../assets/security1.jpg'
-  },
-  { source: './../assets/security2.jpg'
-  },
-  { source: './../assets/security3.jpg'
-  }
-];
+
 
 class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    console.log(this.props.handleClick);
     return (
-      <div className="sidebar float-left col m3 grey lighten-4 height-90 z-depth-1">
-      {media.map ((media, index) =>
-        <MediaPreview media={media} key={index} />
+      <div className="sidebar float-left col s2 grey lighten-4 height-90 z-depth-1">
+      {this.props.media.map ((media, index) =>
+        <MediaPreview handleClick={this.props.handleClick} media={media} key={index} />
       )}
       </div>
     );
