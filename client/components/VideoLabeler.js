cd493df82a2df2bf155db1f1ece7b196e7741c1e
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-04-29 15:34:37 -0700

    add VideoLabeler component

---
+++ client/components/VideoLabeler.js
@@ -0,0 +1,27 @@
+const React = require('react');
+
+class VideoLabeler extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+  
+  render () {
+    return (
+      <div className="row grey lighten-4">
+        <div className="col s8 grey-lighten-2">
+          <div className="frame grey-lighten-1 z-depth-1">
+            <img className="image z-depth-2" src={this.props.video}/>
+          </div>
+        </div>
+        <div className="col s4">
+        </div>
+      </div>
+    ); 
+  }
+}
+
+module.exports = VideoLabeler;
