1a71ceb7c35063bae62116b99d61c08709ea366a
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-04-28 19:25:03 -0700

    create simple Video and Dashboard components

---
+++ client/components/Dashboard.js
@@ -0,0 +1,21 @@
+const React = require('react');
+
+class Dashboard extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render () {
+    return (
+      <div>Hello World</div>
+    );
+  }
+};
+
+
+
+module.exports = Dashboard;
---
+++ client/components/Videos.js
@@ -0,0 +1,22 @@
+const React = require('react');
+
+class Videos extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+  }
+
+  render () {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+
+
+module.exports = Videos;
