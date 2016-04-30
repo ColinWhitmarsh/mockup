da3bafc34a355d840abcfdba60cef2eadbde6834
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-04-29 17:33:18 -0700

    styled ImageLabeler view

--- client/components/ImageLabeler.js
+++ client/components/ImageLabeler.js
@@ -11,15 +11,19 @@ class ImageLabeler extends React.Component {
   
   render () {
     return (
-      <div className="row grey lighten-4">
-        <div className="col s8 grey-lighten-2">
-          <div className="frame grey-lighten-1 z-depth-1">
-            <img className="image z-depth-2" src={this.props.image}/>
+    <div className="imageLabeler no-bottom-margin col s10">
+      <div className="image-frame no-bottom-margin valign-wrapper grey darken-4 z-depth-2">
+        <div className="frame col s12">
+          <div className="frame valign center-align">
+            <img className="frame responsive-img z-depth-2" src={this.props.image}/>
           </div>
         </div>
-        <div className="col s4">
+      </div>
+      <div className="image-toolbar row no-bottom-margin grey lighten-2 z-depth-2">
+        <div className="col s12">
         </div>
       </div>
+    </div>
     ); 
   }
 }
